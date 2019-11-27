b6a0105767a56ce488d213db0f64fb7581c291cc
Darina Kholodova <36265047+DarinaKh@users.noreply.github.com>
2019-11-27 23:40:32 +0200

    Add files via upload

---
+++ PLINQ/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PLINQ
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+        public int Age { get; set; }
+        public float Salary { get; set; }
+    }
+}
---
+++ PLINQ/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace PLINQ
+{
+
+    public class AveradgePerDepartment
+    {
+        public string GroupName { get; set; }
+        public float Value { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stReader = new StreamReader(@"company.csv");
+            var re = new CsvHelper.CsvReader(stReader);
+            var st = new Stopwatch();
+            st.Start();
+
+            var listWithResult = re.GetRecords<Person>()
+                .AsParallel()
+                .GroupBy(p => p.Department)
+                .Select(p =>
+                    new AveradgePerDepartment
+                    {
+                        GroupName = p.Key,
+                        Value = p.Average(s => s.Salary)
+                    })
+                    .AsSequential()
+                    .ToList();
+
+            st.Stop();
+            listWithResult.ForEach(l =>
+            {
+                Console.WriteLine($"{l.GroupName} - {l.Value}");
+            });
+            Console.WriteLine(st.Elapsed.TotalMilliseconds);
+            Console.ReadKey();
+
+
+            stReader = new StreamReader(@"company.csv");
+            re = new CsvHelper.CsvReader(stReader);        
+            var st2 = new Stopwatch();
+            st2.Start();
+
+            var listWithResult2 = re.GetRecords<Person>()
+               .AsParallel()
+               .Select(s => 
+               new
+               {
+                   group = s.Age > 16 && s.Age < 30 ? "17-29" : s.Age >= 30 && s.Age < 46 ? "30-45" : s.Age > 45 ? "45+" : "to young",
+                   person = s
+               })
+               .GroupBy(p => p.group)
+               .Select(p =>
+                   new AveradgePerDepartment
+                   {
+                       GroupName = p.Key,
+                       Value = p.Max(s => s.person.Salary)
+                   })
+                   .AsSequential()
+                   .ToList();
+
+            st2.Stop();
+            listWithResult2.ForEach(l =>
+            {
+                Console.WriteLine($"{l.GroupName} - {l.Value}");
+            });
+            Console.WriteLine(st2.Elapsed.TotalMilliseconds);
+            Console.ReadKey();
+        }
+    }
+}
